82a87a089d03a17c18b521bae0562a39e4f0ae5b
Kia Farhang <kfarhang0@gmail.com>
2016-10-20 16:56:33 -0700

    Slight additions

--- background.js
+++ background.js
@@ -13,6 +13,15 @@ chrome.commands.onCommand.addListener(function(command) {
         } else if (command == 'load_facebook') {
             properties.url = 'https://www.facebook.com';
         }
+        else if (command == 'load_dictionary'){
+            properties.url = 'http://www.dictionary.com';
+        }
+        else if (command == 'load_gmail'){
+            properties.url = 'https://mail.google.com';
+        }
+        // else if (command == 'load_jobs'){
+        //     properties.url = 'http://www.journalismjobs.com';
+        // }
     }
 
 
