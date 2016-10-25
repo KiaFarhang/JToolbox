76c017da39cd075e29b29d832691a792f8c091f1
Kia Farhang <kfarhang0@gmail.com>
2016-10-25 14:28:42 -0700

    Added dictionary search functionality

--- popup.js
+++ popup.js
@@ -43,17 +43,41 @@ function searchGoogle() {
 
 function searchDictionary() {
 
+    clearList();
+
     var text = document.getElementById('wordSearch').value;
+
+    var ul = document.getElementById('definitionZone');
 
     var xhrString = 'https://wordsapiv1.p.mashape.com/words/' + text + '/definitions';
     var key = 'qX2lNxi2wfmshg2v3o2eIDfXOPVTp1RVxaCjsnzhO7HW4yczcC';
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', xhrString);
-    xhr.setRequestHeader('X-Mashape-Authorization', key);
+    var xhr = ajaxCall('GET', xhrString, 'X-Mashape-Authorization', key);
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            alert(this.responseText);
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+
+                var wordArray = JSON.parse(this.responseText).definitions;
+
+                for (var i = 0; i < wordArray.length; i++) {
+                    var thisDefinition = wordArray[i].definition;
+                    var thisPart = wordArray[i].partOfSpeech;
+                    var li = document.createElement('li');
+                    // var span = document.createElement('span');
+
+                    // span.innerText = thisPart;
+
+
+                    // li.appendChild(span);
+                    li.innerText = thisPart + ': ' + thisDefinition;
+                    ul.appendChild(li);
+                }
+            } else {
+                var error = document.createElement('li');
+                error.innerText = 'Sorry, an error occurred. Make sure you spelled the word correctly.';
+                ul.appendChild(error);
+            }
+
         }
     }
 
@@ -63,27 +87,21 @@ function searchDictionary() {
 //Mashape WordsAPI Key qX2lNxi2wfmshg2v3o2eIDfXOPVTp1RVxaCjsnzhO7HW4yczcC   
 
 
-// var links = document.getElementById('searchList').getElementsByTagName('a');
 
-// for (var i = 0; i < links.length; i++) {
-//     links[i].addEventListener('click', toggleInput);
-// }
+function ajaxCall(method, url, optionalHeaderType, optionalHeaderValue) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    if (optionalHeaderType != undefined && optionalHeaderValue != undefined) {
+        xhr.setRequestHeader(optionalHeaderType, optionalHeaderValue);
+    }
+    return xhr;
+}
 
-// function toggleInput(e) {
+function clearList() {
+    var ul = document.getElementById('definitionZone');
 
-//     // if (e.target.nextSibling == null) {
-//     //     // var input = document.createElement('input');
-//     //     // input.setAttribute('type', 'text');
+    while (ul.hasChildNodes()) {
+        ul.removeChild(ul.lastChild);
+    }
 
-//     //     // e.target.parentElement.appendChild(input);
-
-//     //     console.log('work');
-
-//     // } else {
-//     //   console.log('eh');
-//     // }
-
-//     var a = e.target;
-//     alert(a + 'asda');
-
-// }
+}
