1fa7845e2811351944e5f1bfbf4170f69348f130
Kia Farhang <kfarhang0@gmail.com>
2016-10-24 21:20:00 -0700

    Started dictionary search

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ var competitorSearch = document.getElementById('competitorSearch');
 var dictionarySearch = document.getElementById('dictionarySearch');
 
 competitorSearch.addEventListener('submit', searchGoogle);
-dictionarySearch.addEventListener('submit', searchDictionary); 
+dictionarySearch.addEventListener('click', searchDictionary);
 
 function searchGoogle() {
 
@@ -39,31 +39,51 @@ function searchGoogle() {
 
     });
 
+}
 
+function searchDictionary() {
+
+    var text = document.getElementById('wordSearch').value;
+
+    var xhrString = 'https://wordsapiv1.p.mashape.com/words/' + text + '/definitions';
+    var key = 'qX2lNxi2wfmshg2v3o2eIDfXOPVTp1RVxaCjsnzhO7HW4yczcC';
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', xhrString);
+    xhr.setRequestHeader('X-Mashape-Authorization', key);
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+        }
+    }
+
+    xhr.send();
+
+}
 //Mashape WordsAPI Key qX2lNxi2wfmshg2v3o2eIDfXOPVTp1RVxaCjsnzhO7HW4yczcC   
 
 
-    // var links = document.getElementById('searchList').getElementsByTagName('a');
+// var links = document.getElementById('searchList').getElementsByTagName('a');
 
-    // for (var i = 0; i < links.length; i++) {
-    //     links[i].addEventListener('click', toggleInput);
-    // }
+// for (var i = 0; i < links.length; i++) {
+//     links[i].addEventListener('click', toggleInput);
+// }
 
-    // function toggleInput(e) {
+// function toggleInput(e) {
 
-    //     // if (e.target.nextSibling == null) {
-    //     //     // var input = document.createElement('input');
-    //     //     // input.setAttribute('type', 'text');
+//     // if (e.target.nextSibling == null) {
+//     //     // var input = document.createElement('input');
+//     //     // input.setAttribute('type', 'text');
 
-    //     //     // e.target.parentElement.appendChild(input);
+//     //     // e.target.parentElement.appendChild(input);
 
-    //     //     console.log('work');
+//     //     console.log('work');
 
-    //     // } else {
-    //     //   console.log('eh');
-    //     // }
+//     // } else {
+//     //   console.log('eh');
+//     // }
 
-    //     var a = e.target;
-    //     alert(a + 'asda');
+//     var a = e.target;
+//     alert(a + 'asda');
 
-    // }
+// }
