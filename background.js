bbbe2dbe00509202d01737d0687f53cb6158e4f6
Kia Farhang <kfarhang0@gmail.com>
2016-10-26 16:29:32 -0700

    Tweaks + added installation welcome page

--- background.js
+++ background.js
@@ -13,18 +13,16 @@ chrome.commands.onCommand.addListener(function(command) {
         } else if (command == 'load_facebook') {
             properties.url = 'https://www.facebook.com';
         }
-        else if (command == 'load_dictionary'){
-            properties.url = 'http://www.dictionary.com';
-        }
         else if (command == 'load_gmail'){
             properties.url = 'https://mail.google.com';
         }
-        // else if (command == 'load_jobs'){
-        //     properties.url = 'http://www.journalismjobs.com';
-        // }
     }
 
 
 });
+
+// chrome.runtime.onInstalled.addListener(function (object){
+//     chrome.runtime.openOptionsPage();
+// });
 
 
